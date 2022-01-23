0b8e94224d1d185ba4fe1bf195e7bd0ca5a60e28
Marfin Fadhilah <fadilahmarfin@gmail.com>
2022-01-23 21:43:10 +0700

    feat(unit-test): remove unused file

--- app/src/main/java/com/example/todoapp/model/data/TodoList.kt
+++
@@ -1,9 +0,0 @@
-package com.example.todoapp.model.data
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class TodoList (
-    val todoList: List<Todo>
-): Parcelable
